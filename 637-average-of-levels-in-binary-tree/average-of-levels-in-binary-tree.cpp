f601fd8c7a3fefdac81373b72c6246f2782e9919
Nalinapriyavelusamy <163520811+Nalinapriyavelusamy@users.noreply.github.com>
2025-05-28 10:42:54 +0530

    Time: 0 ms (100.00%) | Memory: 24 MB (25.20%) - LeetSync

--- 637-average-of-levels-in-binary-tree/average-of-levels-in-binary-tree.cpp
+++ 637-average-of-levels-in-binary-tree/average-of-levels-in-binary-tree.cpp
@@ -17,21 +17,20 @@ public:
         if(!root)return result;
         q.push(root);
         while(!q.empty()){
-            int levelSize = q.size();
+            int levelsize = q.size();
             double sum = 0;
-            for(int i = 0;i < levelSize;++i){
-                TreeNode* node = q.front();
-                q.pop();
-                sum += node -> val;
-                if(node -> left)
-                q.push(node -> left);
-                if(node -> right)
-                q.push(node -> right);
+            for(int i = 0 ; i<levelsize;i++){
+              TreeNode* node = q.front();
+              q.pop();
+              sum += node -> val;
+                if(node->left)
+                q.push(node->left);
+                if(node->right)
+                q.push(node->right);
             }
-                double average = sum / levelSize;
-                result.push_back(average);
-            }
-            return result;
+            double average = sum / levelsize;
+            result.push_back (average);
         }
-    
+        return result;
+    }
 };
