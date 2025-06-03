d76554f6fd7d76b98268462a6a310d4238b4f374
Nalinapriyavelusamy <163520811+Nalinapriyavelusamy@users.noreply.github.com>
2025-06-03 21:02:21 +0530

    Time: 31 ms (5.21%) | Memory: 101.9 MB (8.68%) - LeetSync

--- 2132-convert-1d-array-into-2d-array/convert-1d-array-into-2d-array.cpp
+++ 2132-convert-1d-array-into-2d-array/convert-1d-array-into-2d-array.cpp
@@ -1,21 +1,17 @@
 class Solution {
 public:
     vector<vector<int>> construct2DArray(vector<int>& original, int m, int n) {
-        // Total elements check
-        if (original.size() != m * n)
-            return {};  // Not possible, return empty 2D array
-
-        vector<vector<int>> result;
-
-        for (int i = 0; i < m; ++i) {
-            vector<int> row;
-            for (int j = 0; j < n; ++j) {
-                // Calculate the index in original array
-                row.push_back(original[i * n + j]);
+        if(original.size()!=m*n){
+            return {};
+        }
+        vector<vector<int>>result;
+        for(int i = 0 ;i<m;i++){
+            vector<int>row;
+            for(int j = 0;j<n;j++){
+                row.push_back(original[i*n+j]);
             }
             result.push_back(row);
         }
-
-        return result;
+            return result;
     }
 };
