d61be1c1285d7534bdf062c0bccc91cb7d4984a2
Nalinapriyavelusamy <163520811+Nalinapriyavelusamy@users.noreply.github.com>
2025-06-06 10:09:39 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (82.45%) - LeetSync

--- 67-add-binary/add-binary.cpp
+++ 67-add-binary/add-binary.cpp
@@ -1,28 +1,27 @@
 class Solution {
 public:
-    string addBinary(string a, string b) {
-        int i = a.size() - 1;
-        int j = b.size() - 1;
-        int carry = 0;
-        string result = "";
+   
+        string addBinary(string a, string b) {
+    string result = "";
+    int i = a.length() - 1, j = b.length() - 1, carry = 0;
 
-        while (i >= 0 || j >= 0 || carry > 0) {
-            int sum = carry;
+    while (i >= 0 || j >= 0 || carry) {
+        int sum = carry;
+       
+        if (i >= 0) 
+        sum += (a[i--] == '1' ? 1 : 0);
+        if (j >= 0) 
+        sum += (b[j--] == '1' ? 1 : 0);
 
-            if (i >= 0) {
-                sum += a[i] - '0';
-                i--;
-            }
 
-            if (j >= 0) {
-                sum += b[j] - '0';
-                j--;
-            }
-
-            carry = sum / 2;
-            result = char((sum % 2) + '0') + result;
-        }
-
-        return result;
+        carry = sum / 2;
+        char bit = (sum % 2 == 1) ? '1' : '0';
+result.push_back(bit);
+// Append current bit
     }
+
+    reverse(result.begin(), result.end());  // Reverse the result string
+    return result;
+}
+    
 };
