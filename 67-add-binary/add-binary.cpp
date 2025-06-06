ba6d4e9aebabce9a76b3c1a73e6963d4470b9348
Nalinapriyavelusamy <163520811+Nalinapriyavelusamy@users.noreply.github.com>
2025-06-06 10:36:43 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (42.43%) - LeetSync

--- 67-add-binary/add-binary.cpp
+++ 67-add-binary/add-binary.cpp
@@ -1,27 +1,21 @@
 class Solution {
 public:
-   
-        string addBinary(string a, string b) {
-    string result = "";
-    int i = a.length() - 1, j = b.length() - 1, carry = 0;
-
-    while (i >= 0 || j >= 0 || carry) {
-        int sum = carry;
-       
-        if (i >= 0) 
-        sum += (a[i--] == '1' ? 1 : 0);
-        if (j >= 0) 
-        sum += (b[j--] == '1' ? 1 : 0);
-
-
-        carry = sum / 2;
-        char bit = (sum % 2 == 1) ? '1' : '0';
-result.push_back(bit);
-// Append current bit
+    string addBinary(string a, string b) {
+        int i = a.length()-1;
+        int j = b.length()-1;
+        int carry = 0;
+        string result = "";
+        while(i>=0||j>=0||carry){
+            int sum = carry;
+            if(i>=0)
+            sum = sum+(a[i--]=='1'?1:0);
+             if(j>=0)
+            sum = sum+(b[j--]=='1'?1:0);
+            carry = sum/2;
+            char bit = (sum%2==1)?'1':'0';
+          result.push_back(bit);
+        }
+        reverse(result.begin(),result.end());
+        return result;
     }
-
-    reverse(result.begin(), result.end());  // Reverse the result string
-    return result;
-}
-    
 };
